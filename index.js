ae4852cdb15ffa7936151ca599e73be0a1330dbe
aleion <alejandro.ion.carvajal@gmail.com>
2020-05-30 00:32:06 +0300

    responsive nav bar done

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ var animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'images/index/animation/data2.json'
 });
+
+
+// Burger menu
+
+const navSlider = () => {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.nav-menu');
+    const navLinks = document.querySelectorAll('.nav-menu a');
+
+    burger.addEventListener('click',() => {
+      menu.classList.toggle('nav-active');
+
+      navLinks.forEach((link, index) => {
+        if(link.style.animation){
+          link.style.animation = '';
+        }else{
+          link.style.animation = `navLinksFade 0.4s ease-in-out forwards ${index / 7 + .3}s`;
+        }
+      });
+
+      burger.classList.toggle('cross');
+
+    });
+
+    
+  };
+
+  navSlider();
