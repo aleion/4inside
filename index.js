b157bb638658e2795373c1c1d70f4410783cf233
aleion <alejandro.ion.carvajal@gmail.com>
2020-05-29 21:04:22 +0300

    preparing files for index

---
+++ index.js
@@ -0,0 +1,15 @@
+const animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'images/index/animation/data.json'
+  });
+
+const animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'images/index/animation/data2.json'
+});
